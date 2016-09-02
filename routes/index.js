45092386662c6acfe34b956fb763d31a189379d0
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-02 11:06:33 -0300

    final

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const useragent = require('express-useragent');
+
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res)=> {
+
+    const soft = useragent.parse(req.headers['user-agent']);
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const lang = req.headers['accept-language'].split(',')[0];
+
+    console.log( JSON.stringify(soft));
+    res.end( `{"ipaddress": "${ip}", "language" : "${lang}" , "software": "${soft.platform} ${soft.os}"}`)
 });
 
 module.exports = router;
