b7704ce1f88569ab6c7a34011888e115938a14d2
JEAN P DE ROGATIS <jrogatis@conecta.net.br>
2016-09-02 11:08:13 -0300

    simple ajust on layout

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,6 @@ router.get('/', (req, res)=> {
     const soft = useragent.parse(req.headers['user-agent']);
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const lang = req.headers['accept-language'].split(',')[0];
-
-    console.log( JSON.stringify(soft));
     res.end( `{"ipaddress": "${ip}", "language" : "${lang}" , "software": "${soft.platform} ${soft.os}"}`)
 });
 
